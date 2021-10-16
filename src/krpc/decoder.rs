d1113f914c9e595f85f55c4bbe969dd8450cd87b
Bart Slinger <bartslinger@gmail.com>
2021-10-16 10:25:03 +0200

    decode uint32

--- src/krpc/decoder.rs
+++ src/krpc/decoder.rs
@@ -10,7 +10,7 @@ mod tests {
         let decoded = decode_float(input).unwrap();
         assert_eq!(expected, decoded);
     }
-    
+
     #[test]
     fn test_double() {
         let input = vec![247, 208, 219, 17, 159, 66, 66, 65];
@@ -18,25 +18,35 @@ mod tests {
         let decoded = decode_double(input).unwrap();
         assert_eq!(expected, decoded);
     }
+    
+    #[test]
+    fn test_uint32() {
+        let input = vec![0xFA, 0xBC, 0x7E];
+        let expected = 2072186;
+        let decoded = decode_uint32(input).unwrap();
+        assert_eq!(expected, decoded);
+    }
 }
 
 pub fn decode_float(input: Vec<u8>) -> Result<f32, Error> {
     let array: Result<[u8; 4], _> = input.try_into();
-    Ok(
-        f32::from_le_bytes(
-            array.or(Err(Error::InvalidInput))?
-        )
-    )
+    Ok(f32::from_le_bytes(array.or(Err(Error::InvalidInput))?))
 }
 
 pub fn decode_double(input: Vec<u8>) -> Result<f64, Error> {
     let array: Result<[u8; 8], _> = input.try_into();
-    Ok(
-        f64::from_le_bytes(
-            array.or(Err(Error::InvalidInput))?
-        )
-    )
+    Ok(f64::from_le_bytes(array.or(Err(Error::InvalidInput))?))
 }
+
+pub fn decode_uint32(mut input: Vec<u8>) -> Result<u32, Error> {
+    let res = prost::encoding::decode_varint(&mut &*input)?;
+    let output = res as u32;
+    Ok(output)
+}
+// BOOL = 7;
+// STRING = 8;
+// BYTES = 9;
+
 
 #[derive(Debug)]
 pub enum Error {
